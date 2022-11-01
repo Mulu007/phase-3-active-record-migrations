e1abe0c47b18df01ad4ca8851e70ef8b7710bb87
Mulu <brianmulu007@gmail.com>
2022-11-01 20:53:29 +0300

    Done

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,4 @@
+# RACK_ENV is a specific environment variable that is used by the sinatra-activerecord gem to determine which database to connect to: in our environment.rb file, we're specifying that it should use the development database, which is configured in the database.yml file.
 ENV["RACK_ENV"] ||= "development"
 
 require 'bundler/setup'
